913c1917cd54fdaf2734c3fe98c80c5cb9146394
Manel1804 <abakhouchmanel@gmail.com>
2024-05-28 20:33:35 +0100

    feat: add config

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import Models.Article;
+import Services.ServiceArticle;
+import UTILS.MyConfig;
+
+import java.sql.Date;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        MyConfig m= MyConfig.getInstance();
+       System.out.printf("Hello and welcome!");
+       ServiceArticle sa = new ServiceArticle();
+       Article a1 = new Article(new Date(2002,12,23),"objet","produit1");
+        sa.add(a1);
+
+    }
+}
