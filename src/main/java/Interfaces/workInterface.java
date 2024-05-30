b439e4728257cce502b19fd861841c79afa45738
Molk SAOUABI <molk.saouabi@wecraft.tn>
2024-05-30 21:08:29 +0100

    fix: add update and delete to Service interface

--- src/main/java/Interfaces/workInterface.java
+++ src/main/java/Interfaces/workInterface.java
@@ -5,5 +5,7 @@ import java.util.List;
 public interface workInterface<T> {
     void add(T t);
     List<T> getAll();
+    void update(T t);
+    void delete(T t);
 
 }
--- src/main/java/Models/Cours.java
+++ src/main/java/Models/Cours.java
@@ -18,6 +18,7 @@ public class Cours extends Entite {
         this.video = video;
     }
 
+
     public void publier() {
 
     }
