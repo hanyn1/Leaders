161845f9e1464fe8291a129b83415c9c4699112b
Molk SAOUABI <molk.saouabi@wecraft.tn>
2024-05-30 22:43:32 +0100

    fix: add cours

--- src/main/java/Interfaces/workInterface.java
+++ src/main/java/Interfaces/workInterface.java
@@ -1,11 +1,12 @@
 package Interfaces;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface workInterface<T> {
     void add(T t);
     List<T> getAll();
-    void update(T t);
+    void update(T t) throws SQLException;
     void delete(T t);
 
 }
