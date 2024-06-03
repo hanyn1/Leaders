36a9624c036354f83c14ecaaeb25de421071877c
Molk SAOUABI <molk.saouabi@wecraft.tn>
2024-06-04 00:57:17 +0100

    feat: add class Badge

---
+++ src/main/java/Models/Badge.java
@@ -0,0 +1,35 @@
+package Models;
+
+public class Badge {
+    private int id;
+    private String nom;
+
+    public Badge() {
+    }
+
+    public Badge(String nom) {
+        this.nom = nom;
+    }
+
+    public Badge(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+}
