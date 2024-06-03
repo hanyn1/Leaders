128a32e0d779cbe4ae66d01328c4699319da0a0c
Molk SAOUABI <molk.saouabi@wecraft.tn>
2024-06-03 19:50:49 +0100

    add main and home javafx

---
+++ src/main/java/tests/Home.java
@@ -0,0 +1,28 @@
+package tests;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Home  extends Application {
+    public static void  main(String[]args) { launch(args);}
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("src/main/resources/AjouterCours.fxml"));
+        try {
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Ajouter cours");
+            primaryStage.show();
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
