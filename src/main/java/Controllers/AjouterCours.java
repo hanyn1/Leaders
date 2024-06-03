af4031974b86709452aaebbe3e509e81d9525ce6
Molk SAOUABI <molk.saouabi@wecraft.tn>
2024-06-03 20:02:57 +0100

    add fx contoller

--- src/main/java/Controllers/AjouterCours.java
+++ src/main/java/Controllers/AjouterCours.java
@@ -1,4 +1,22 @@
 package Controllers;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class AjouterCours {
+
+    @FXML
+    private Button fxajouterbtn;
+
+    @FXML
+    private TextField fxdescription;
+
+    @FXML
+    private TextField fxtitre;
+
+    @FXML
+    private TextField fxvideo;
+
 }
