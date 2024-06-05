5ca18a5de406fbcf5893f84e185eb024eaea5e1a
elwehdiabir <elwehdiabir@gmail.com>
2024-06-05 12:10:04 +0200

    last version

---
+++ src/main/java/test/home.java
@@ -0,0 +1,15 @@
+package test;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class home extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
