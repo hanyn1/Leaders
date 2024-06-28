7e687b43d44997dab06d93a5d9fa35989d4701f3
Molk SAOUABI <molk.saouabi@wecraft.tn>
2024-06-28 16:52:37 +0100

    add ajouter cours admin

--- src/main/java/Controllers/AdminDashboardController.java
+++ src/main/java/Controllers/AdminDashboardController.java
@@ -1,17 +1,50 @@
 package Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class AdminDashboardController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     public void switchForm(ActionEvent actionEvent) {
     }
 
-    public void goToCoursesList(ActionEvent actionEvent) {
+    @FXML
+    public void goToCoursesList(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/CoursList.fxml")));
+        stage =(Stage)( (Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void close(ActionEvent actionEvent) {
     }
 
     public void minimize(ActionEvent actionEvent) {
+    }
+
+    public void addCours(ActionEvent actionEvent) {
+    }
+
+    public void uploadImage(ActionEvent actionEvent) {
+    }
+
+    public void uploadVideo(ActionEvent actionEvent) {
+    }
+
+    public void deleteCours(ActionEvent actionEvent) {
+    }
+
+    public void updateCours(ActionEvent actionEvent) {
     }
 }
