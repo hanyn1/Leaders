e7c19a8de47218be4834e51606531dccb6e2fc44
Molk SAOUABI <molk.saouabi@wecraft.tn>
2024-06-28 12:18:25 +0100

    fix: landing window

--- src/main/java/Controllers/EtudiantDashboardController.java
+++ src/main/java/Controllers/EtudiantDashboardController.java
@@ -1,17 +1,42 @@
 package Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class EtudiantDashboardController {
+    @FXML
+    private AnchorPane main_form;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     public void close(ActionEvent actionEvent) {
+        System.exit(0);
     }
 
     public void minimize(ActionEvent actionEvent) {
+        Stage stage = (Stage) main_form.getScene().getWindow();
+        stage.setIconified(true);
     }
 
     public void switchForm(ActionEvent actionEvent) {
     }
 
-    public void goToCoursesList(ActionEvent actionEvent) {
+    @FXML
+    public void goToCoursesList(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/EtudiantCoursList.fxml")));
+        stage =(Stage)( (Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
--- src/main/java/Controllers/VisitorController.java
+++ src/main/java/Controllers/VisitorController.java
@@ -6,15 +6,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.Objects;
+
 
 public class VisitorController {
     @FXML
     private AnchorPane main_form;
+    @FXML
+    private ImageView imageview;
+
+
     public void close(ActionEvent actionEvent) {
         System.exit(0);
     }
