c2bb5186a2946f5d4ff528e583f611ca0a319a20
hanyn1 <hanine.azizi@esprit.tn>
2024-06-29 12:24:39 +0200

    crud/interface

--- src/main/java/tests/Home.java
+++ src/main/java/tests/Home.java
@@ -1,32 +1,28 @@
 package tests;
 
+import Controllers.FormationController;
+import Services.ServiceFormation;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Home extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FormationController.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        FormationController controller = loader.getController();
+        controller.setServiceFormation(new ServiceFormation()); // Set the service
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Formation Management");
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
-
-    @Override
-    public void start(Stage primaryStage) {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/RoleCRUD.fxml"));
-        try {
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-    }
-
 }
